4f52556e5e1793c384553c5b183fd366b1594009
Ander Belloso <ander.belloso@opendeusto.es>
2026-04-03 14:08:59 +0200

    Basic movement added
    WASD moves and jump

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int movSpeed = 15;
+    public float fuerzaSalto = 5f;
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        move();
+        jump();
+    }
+
+    private void move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += Vector3.left * Time.deltaTime * movSpeed;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.position += Vector3.forward * Time.deltaTime * movSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.position += Vector3.back * Time.deltaTime * movSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += Vector3.right * Time.deltaTime * movSpeed;
+        }
+    }
+
+    private void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+        }
     }
 }
